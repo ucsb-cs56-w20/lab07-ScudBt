62585f2d9626b68ffd84853e57e883bff253c0cf
ScudBt <huiyuzhang@ucsb.edu>
2020-02-11 18:35:52 -0800

    hz - added form for search at url /earthquakes/search

---
+++ src/main/java/edu/ucsb/cs56/w20/lab07/formbeans/EqSearch.java
@@ -0,0 +1,25 @@
+package edu.ucsb.cs56.w20.lab07.formbeans;
+
+public class EqSearch {
+    private int distance;
+    private int minmag;
+
+    public EqSearch() {}
+    
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getMinmag() {
+        return minmag;
+    }
+
+    public void setMinmag(int minmag) {
+        this.minmag = minmag;
+    }
+    
+}
