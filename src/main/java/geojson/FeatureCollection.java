aa4695ee07772727ba74f4540d859cf2429d013e
ScudBt <huiyuzhang@ucsb.edu>
2020-02-17 16:37:47 -0800

    new Java obj metadata is created

--- src/main/java/geojson/FeatureCollection.java
+++ src/main/java/geojson/FeatureCollection.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 public class FeatureCollection {
     private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
     public String type;
+    public Metadata metadata;
 
     public static FeatureCollection fromJSON(String json) {
         try {
